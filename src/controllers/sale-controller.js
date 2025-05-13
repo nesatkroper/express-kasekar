fd3f914d08ed0598e367f52708c75e0b7e5992d4
phanunsuon <phanunsuon@gmail.com>
2025-05-13 05:01:45 +0000

    modified:   package.json 	new file:   prisma/migrations/20250513042804_drop_stockmovement/migration.sql 	modified:   prisma/schema.prisma 	modified:   src/controllers/sale-controller.js

--- src/controllers/sale-controller.js
+++ src/controllers/sale-controller.js
@@ -1,3 +1,5 @@
+const prisma = require("@/provider/client");
+const path = require("path");
 const {
   baseSelect,
   baseCreate,
@@ -5,6 +7,7 @@ const {
   basePatch,
   baseDestroy,
 } = require("../utils");
+const { data } = require("@/middleware/app-logger-middleware");
 
 const model = "sale";
 
@@ -28,16 +31,68 @@ const select = async (req, res) => {
 };
 
 const create = async (req, res) => {
-  try {
-    const data = { ...req.body };
+  await prisma.$transaction(async (tx) => {
+    try {
+      const picture = req.file ? path.basename(req.file.path) : null;
+      const { employeeId, customerId, saleDate, amount, memo, cartItems } = req.body;
 
-    const result = await baseCreate(model, data);
-    return res.status(201).json(result);
-  } catch (err) {
-    console.error(`Error creating ${model}:`, err);
-    return res.status(500).json({ error: `Error :${err}` });
-  }
-};
+      const sale = await tx.sale.create({
+        data: {
+          employeeId,
+          customerId,
+          saleDate,
+          amount,
+          memo,
+          picture,
+          status: "active"
+        }
+      });
+
+      for (const item of cartItems) {
+        await tx.saledetail.create({
+          data: {
+            saleId: sale.saleId,
+            productId: item.productId,
+            quantity: item.quantity,
+            amount: item.amount,
+            memo: item.memo,
+            status: "active"
+          }
+        });
+
+        await tx.stock.update({
+          where: {
+            productId: item.productId
+          },
+          data: {
+            quantity: {
+              decrement: item.quantity
+            },
+            memo: "Sale",
+            status: "active"
+          }
+        });
+
+      };
+
+
+    } catch (err) {
+      console.log(err)
+    }
+  });
+}
+
+// const create = async (req, res) => {
+//   try {
+//     const data = { ...req.body };
+
+//     const result = await baseCreate(model, data);
+//     return res.status(201).json(result);
+//   } catch (err) {
+//     console.error(`Error creating ${model}:`, err);
+//     return res.status(500).json({ error: `Error :${err}` });
+//   }
+// };
 
 const update = async (req, res) => {
   try {
