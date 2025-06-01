d21e7e5dc1d456321fae0dfbcea801327b427c8a
megalobob <phanunsuon@gmail.com>
2025-06-01 16:18:50 +0700

    modified:   src/constants/cors.js

--- src/constants/cors.js
+++ src/constants/cors.js
@@ -1,9 +1,7 @@
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:3000",
   "http://74.50.67.162",
-  "http://74.50.67.162:3100",
-  "http://74.50.67.162:4100",
+  "http://localhost:5173",
+  "https://huotsopheaksakana.site"
 ];
 
 const allowedHeaders = [
